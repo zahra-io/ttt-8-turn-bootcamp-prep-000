550d415f2a90da924ece06b27421ef26aab019e4
Zahra Khan <zarssskhan@gmail.com>
2017-10-05 08:48:41 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,6 +12,14 @@ def valid_move?(board, index)
   end
 end
 
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
 def input_to_index(user_input)
   user_input.to_i - 1
 end
