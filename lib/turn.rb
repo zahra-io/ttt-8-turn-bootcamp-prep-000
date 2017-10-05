f2a71f82b59260eb25eb43020e9b32c276b8a77e
Zahra Khan <zarssskhan@gmail.com>
2017-10-05 09:00:26 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -29,14 +29,14 @@ def move(board, index, current_player = "X")
   board[index] = current_player
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end 
-end
+# def turn(board)
+#   puts "Please enter 1-9:"
+#   input = gets.strip
+#   index = input_to_index(input)
+#   if valid_move?(board, index)
+#     move(board, index)
+#     display_board(board)
+#   else
+#     turn(board)
+#   end 
+# end
