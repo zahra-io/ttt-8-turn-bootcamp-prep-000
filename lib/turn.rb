362cf66582d13633f43315e1775e1b40e2e63de7
Zahra Khan <zarssskhan@gmail.com>
2017-10-05 08:47:33 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,3 +8,11 @@ end
 
 def valid_move?(board, index)
 end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, current_player = "X")
+  board[index] = current_player
+end
